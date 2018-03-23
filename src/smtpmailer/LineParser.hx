fe6d59946bc826a168153e9908d469fcf0f8cec9
Kevin Leung <kevinresol@gmail.com>
2018-03-23 17:39:21 +0800

    Use Splitter

--- src/smtpmailer/LineParser.hx
+++ src/smtpmailer/LineParser.hx
@@ -2,27 +2,8 @@ package smtpmailer;
 
 import tink.io.StreamParser;
 
-class LineParser extends BytewiseParser<String> {
-  
-	var buf: StringBuf = new StringBuf();
-	
-	public function new() {}
-	
-	override function read(c: Int): ParseStep<String> {
-		return switch c {
-			case 10:
-				var ret = buf.toString();
-				if (ret.charCodeAt(ret.length-1) == 13) 
-					ret = ret.substr(0, -1);
-				if (ret == '')
-					Progressed;
-				else {
-					buf = new StringBuf();
-					Done(ret);
-				}
-			default:
-				buf.addChar(c);
-				Progressed;
-		}
+class LineParser extends Splitter {
+	public function new() {
+		super('\r\n');
 	}
 }
